5c2bcad10235ce13bb7dc9061d0d7705a5b18ab6
simonsavine <simon.antoine.savine@gmail.com>
2026-07-16 22:39:34 +0200

    initial commit

---
+++ IntroText.py
@@ -0,0 +1,19 @@
+import pygame
+import pygame_gui
+
+pygame.init()
+
+pygame.display.set_caption("Intro")
+
+# Change resolution to fit your monitor resolution.
+WIDTH, HEIGHT = 1400, 800
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+
+
+# Not entirely sure this is needed
+CLOCK = pygame.time.Clock()
+
+font = pygame.font.SysFont('freesans', 75)
+
+pygame.display
+
