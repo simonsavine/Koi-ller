dc35fce27751a04440433edeae82d17ec4bb99d3
simonsavine <simon.antoine.savine@gmail.com>
2026-07-17 19:02:18 +0200

    Updates

--- IntroText.py
+++ IntroText.py
@@ -1,5 +1,13 @@
 import pygame
 import pygame_gui
+import sys
+import time
+
+def typewriter_effect(text):
+    for char in text:
+        print(char, end="")
+        sys.stdout.flush()
+        time.sleep(0.1)
 
 pygame.init()
 
@@ -9,13 +17,15 @@ pygame.display.set_caption("Intro")
 WIDTH, HEIGHT = 1400, 800
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 
-
 # Not entirely sure this is needed
 CLOCK = pygame.time.Clock()
 
 font = pygame.font.SysFont('freesans', 75)
 
-pygame.display()
+box_width, box_height = WIDTH * 0.375, HEIGHT * 0.075
+
+rect = pygame.Rect(0, 0, box_width, box_height)
+rect.center = (WIDTH / 2, HEIGHT / 2)
 
 # Looooong text incoming:
 
@@ -23,13 +33,15 @@ IntroText = font.render(
     "You are a killer koi fish, thirsy for revenge and angered by the" \
     "the fishy peacemakers that rule the bottom of the ocean. You are equipped with a gun" \
     "that can richochet off of walls and an immaculate sense of ballistic physics. Let them know" \
-    "your rage!"
+    "your rage!", True, (80, 80, 80)
 )
 
-SCREEN.blit(
+# Infinite loop for display:
 
-
-)
+running = True
+while running:
+    SCREEN.blit(IntroText, rect)
+    running = False
 
 pygame.display
 
