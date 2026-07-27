42bcdbe01b133d46174e512c85e50e137d3ae4bb
simonsavine <simon.antoine.savine@gmail.com>
2026-07-27 17:08:12 +0200

    Loading bar.py created, newline detection in intro text.

--- IntroText.py
+++ IntroText.py
@@ -1,4 +1,5 @@
 import pygame
+import ptext
 
 pygame.init()
 
@@ -10,14 +11,16 @@ WIDTH, HEIGHT = 1400, 800
 font = pygame.font.Font('freesansbold.ttf', 24)
 screen = pygame.display.set_mode([WIDTH, HEIGHT], pygame.RESIZABLE)
 timer = pygame.time.Clock()
-message = "You are a killer koi fish, thirsty for revenge and angered by the " \
-    "fishy peacemakers that rule the bottom of the ocean. You are equipped with a gun " \
-    "that can richochet off of walls and an immaculate sense of ballistic physics. Let them know " \
-    "your rage! Press ENTER to continue.."
+message = """You are a killer koi fish, thirsty for revenge and angered by the fishy peacemakers that rule the bottom of the ocean. You are equipped with a gun that can richochet off of walls and an immaculate sense of ballistic physics. Let them know your rage!
+
+
+
+ Press ENTER to continue.. """
 snip = font.render('', True, 'white')
 counter = 0
 speed = 1
 done = False
+
 
 running = True
 while running:
@@ -32,10 +35,6 @@ while running:
 
     # The first letter to cross the WIDTH threshold goes to next line:
 
-    for char in message:
-        if font.size(char)[0] < WIDTH:
-            # blit next part of message on next line
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -44,9 +43,13 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                running = False
 
-    snip = font.render(message[0:counter//speed], True, 'black')
-    screen.blit(snip, (0, 0))
+
+    snip = message[0:counter//speed]
+    ptext.draw(snip, (0, WIDTH / 11), width = WIDTH, color = 'black', fontsize = 75)
 
     pygame.display.flip()
 
