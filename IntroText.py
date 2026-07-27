2944d7fa3969d93d6539ebcafa9a1d0a52defa6a
simonsavine <simon.antoine.savine@gmail.com>
2026-07-27 16:02:00 +0200

    Intro text getting somewhere

--- IntroText.py
+++ IntroText.py
@@ -1,61 +1,48 @@
 import pygame
-import pygame_gui
-import sys
-import time
-
-def typewriter_effect(text):
-    for char in text:
-        print(char, end="")
-        sys.stdout.flush()
-        time.sleep(0.1)
 
 pygame.init()
 
 pygame.display.set_caption("Intro")
 
-# Change resolution to fit your monitor resolution.
+# Resize as you desire:
 WIDTH, HEIGHT = 1400, 800
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT), pygame.RESIZABLE)
 
-# Not entirely sure this is needed
-CLOCK = pygame.time.Clock()
-
-font = pygame.font.SysFont('freesans', 75)
-
-box_width, box_height = WIDTH * 0.375, HEIGHT * 0.075
-
-rect = pygame.Rect(0, 0, box_width, box_height)
-rect.center = (WIDTH / 2, HEIGHT / 2)
-
-# Looooong text incoming:
-
-IntroText = font.render(
-    "You are a killer koi fish, thirsy for revenge and angered by the" \
-    "fishy peacemakers that rule the bottom of the ocean. You are equipped with a gun" \
-    "that can richochet off of walls and an immaculate sense of ballistic physics. Let them know" \
-    "your rage! Press ENTER to continue..", True, (80, 80, 80)
-)
-
-text_rect = IntroText.get_rect()
-
-# Infinite loop for display:
+font = pygame.font.Font('freesansbold.ttf', 24)
+screen = pygame.display.set_mode([WIDTH, HEIGHT], pygame.RESIZABLE)
+timer = pygame.time.Clock()
+message = ("You are a killer koi fish, thirsty for revenge and angered by the " \
+    "fishy peacemakers that rule the bottom of the ocean. You are equipped with a gun " \
+    "that can richochet off of walls and an immaculate sense of ballistic physics. Let them know " \
+    "your rage! Press ENTER to continue..")
+snip = font.render('', True, 'white')
+counter = 0
+speed = 3
+done = False
 
 running = True
 while running:
-    SCREEN.blit(IntroText, text_rect)
-    running = False
 
-    # Upon pressing ENTER, the player goes to the next screen (Main game loop)
+    screen.fill('white')
+    timer.tick(60)
+    pygame.draw.rect(screen, 'black', [0, 300, 800, 200])
+
+    if counter < speed * len(message):
+        counter += 1
+    elif counter >= speed * len(message):
+        done = True
 
     for event in pygame.event.get():
-
-        if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
+        if event.type == pygame.QUIT:
             running = False
             
         elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-                    exit()
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
-pygame.display
+
+    snip = font.render(message[0:counter//speed], True, 'white')
+    screen.blit(snip, (10, 310))
+
+    pygame.display.flip()
+
 
