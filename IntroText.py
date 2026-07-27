3fa84419c1a28065f456b2f991da49be60336c39
simonsavine <simon.antoine.savine@gmail.com>
2026-07-27 16:18:21 +0200

    More improvements Intro text

--- IntroText.py
+++ IntroText.py
@@ -10,13 +10,13 @@ WIDTH, HEIGHT = 1400, 800
 font = pygame.font.Font('freesansbold.ttf', 24)
 screen = pygame.display.set_mode([WIDTH, HEIGHT], pygame.RESIZABLE)
 timer = pygame.time.Clock()
-message = ("You are a killer koi fish, thirsty for revenge and angered by the " \
+message = "You are a killer koi fish, thirsty for revenge and angered by the " \
     "fishy peacemakers that rule the bottom of the ocean. You are equipped with a gun " \
     "that can richochet off of walls and an immaculate sense of ballistic physics. Let them know " \
-    "your rage! Press ENTER to continue..")
+    "your rage! Press ENTER to continue.."
 snip = font.render('', True, 'white')
 counter = 0
-speed = 3
+speed = 1
 done = False
 
 running = True
@@ -24,24 +24,29 @@ while running:
 
     screen.fill('white')
     timer.tick(60)
-    pygame.draw.rect(screen, 'black', [0, 300, 800, 200])
 
     if counter < speed * len(message):
         counter += 1
     elif counter >= speed * len(message):
         done = True
 
+    # The first letter to cross the WIDTH threshold goes to next line:
+
+    for char in message:
+        if font.size(char)[0] < WIDTH:
+            # blit next part of message on next line
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
 
-    snip = font.render(message[0:counter//speed], True, 'white')
-    screen.blit(snip, (10, 310))
+    snip = font.render(message[0:counter//speed], True, 'black')
+    screen.blit(snip, (0, 0))
 
     pygame.display.flip()
 
